987a41aa6638b5fcd2dad094f5c28bbd5fe57e9f
dverma9165 <dverma9165@gmail.com>
2026-06-29 11:03:24 +0530

    Actual Qty Fetching changed

--- src/components/receipt-check.jsx
+++ src/components/receipt-check.jsx
@@ -154,7 +154,7 @@ const PROCESSED_RECEIPTS_COLUMNS_META = [
   { header: "Party Name", dataKey: "vendorName", toggleable: true },
   { header: "Product Name", dataKey: "rawMaterialName", toggleable: true },
   { header: "PO Qty", dataKey: "qty", toggleable: true },
-  { header: "ACTUAL Qty", dataKey: "liftingQty", toggleable: true },
+  { header: "ACTUAL Qty", dataKey: "actualQuantity_fromSheet", toggleable: true },
   {
     header: "Bill Copy",
     dataKey: "billCopy",
